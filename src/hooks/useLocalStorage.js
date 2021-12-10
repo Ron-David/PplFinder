eda9030ee744f34d988729f60d37c32aecb8a342
your <77014885+Ron-David@users.noreply.github.com>
2021-12-10 03:18:08 +0200

    fixing localstorage bug

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export const useLocalStorage = (key) => {
-    const [getStorage, setLocalStoreData] = useState(JSON.parse(window.localStorage.getItem(key)));
+    const [getStorage, setLocalStoreData] = useState([]);
 
     const setStorage = data => {
         window.localStorage.setItem(key, JSON.stringify(data))
